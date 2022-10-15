c5b90ab2f627709479a3af735771298541cb23ff
yhz <1990507723@qq.com>
2022-10-14 17:55:09 -0700

    rename tests and add stub to prob.jl

--- src/prob.jl
+++ src/prob.jl
@@ -6,26 +6,25 @@ abstract type spProblem end
 
 """
 `solve_subproblem(prob, in_state, obs)` solves the subproblem given input state
-and observation. Function should return a structure (#TODO) giving the solution
+and observation. Function should return a structure giving the solution
 of the subproblem.
 """
-function get_problem(prob::spProblem, stage::Int, in_state, obs) end
+function get_stage_problem(prob::spProblem, stage::Int, in_state, obs)
+    error("Unimplemented")
+end
 
 """
 Evaluate the stage problem of prob at a given input state in_state
 and dual point, and an observation.
 """
 function evaluate_dual_subproblem(prob::spProblem, stage::Int,
-    in_state, dual_point, obs) end
+    in_state, dual_point, obs)
+    error("Unimplemented")
+end
 
 """
 Get (sub)gradient of primal objective, ignoring the constraints.
 """
-function objective_grad(prob::spProblem, stage::Int, primal_point) end
-
-# TODO: design randomness struct
-# abstract type spRandomness end
-
-# struct smps_randomness <: spRandomness
-
-# end
+function objective_grad(prob::spProblem, stage::Int, primal_point)
+    error("Unimplemented")
+end

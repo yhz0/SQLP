d2dc5e4c4861f77995b8b398a9e04ff4f49ce56b
yhz <1990507723@qq.com>
2023-01-23 17:11:09 -0800

    remove projection

--- src/SQLP.jl
+++ src/SQLP.jl
@@ -2,7 +2,6 @@ module SQLP
     include("utils.jl")
     include("prob.jl")
     include("smps/smps.jl")
-    include("projection.jl")
 
     # Algorithms
     include("sd_algorithm/sd_algorithm.jl")
